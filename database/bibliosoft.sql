659037797550226d2951ab21c290eb3bb15444fe
gigenaMatias <matiasgigena12345@outlook.com>
2021-05-24 04:50:01 -0300

    backup de BD

--- database/bibliosoft.sql
+++ database/bibliosoft.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2021 a las 23:13:51
+-- Tiempo de generación: 24-05-2021 a las 09:48:11
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.34
 
@@ -28,15 +28,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL,
-  `categoria` varchar(100) NOT NULL
+  `id_categoria` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`id`, `categoria`) VALUES
+INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
 (1, 'comic'),
 (2, 'autografia'),
 (3, 'cientificos'),
@@ -58,7 +58,7 @@ INSERT INTO `categorias` (`id`, `categoria`) VALUES
 
 CREATE TABLE `libros` (
   `nombre` varchar(150) NOT NULL,
-  `id` int(11) NOT NULL,
+  `id_libro` int(11) NOT NULL,
   `autor` varchar(100) NOT NULL,
   `categoria` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
@@ -69,12 +69,12 @@ CREATE TABLE `libros` (
 -- Volcado de datos para la tabla `libros`
 --
 
-INSERT INTO `libros` (`nombre`, `id`, `autor`, `categoria`, `descripcion`, `imagen`) VALUES
-('el señor de los anillos', 1, 'j. r. r. tolkien', 11, 'La novela narra el viaje del protagonista principal, Frodo Bolsón, hobbit de la Comarca, para destruir el Anillo Único y la consiguiente guerra que provocará el enemigo para recuperarlo, ya que es la ', 'https://ibb.co/FkpC4sH'),
-('harry potter y la piedra filosofal', 2, 'j. k. rowling', 11, 'Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y el insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vid', 'https://ibb.co/QkvdL4G'),
-('el codigo da vinci', 3, 'dan brown', 11, 'El Código da Vinci es una novela de suspenso escrita por Dan Brown, esta obra aumentó el interés sobre la especulación del santo grial además cuestionó el papel que se le dio a María Magdalena en el c', 'https://ibb.co/DC6NHRF'),
-('la biblia', 4, 'armando j. levoratti', 4, 'libro del pueblo de dios', 'https://ibb.co/m4FK4Pk'),
-('crepusculo', 5, 'stephenie meyer', 12, 'esta es la historia de Bella, una chica que luego de enamorarse descubre un mundo de vampiros completamente desconocido.', 'https://ibb.co/yB61VHL');
+INSERT INTO `libros` (`nombre`, `id_libro`, `autor`, `categoria`, `descripcion`, `imagen`) VALUES
+('el señor de los anillos', 1, 'j. r. r. tolkien', 11, 'La novela narra el viaje del protagonista principal, Frodo Bolsón, hobbit de la Comarca, para destruir el Anillo Único y la consiguiente guerra que provocará el enemigo para recuperarlo, ya que es la ', 'images/anillo.jpg'),
+('harry potter y la piedra filosofal', 2, 'j. k. rowling', 11, 'Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y el insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vid', 'images/harry.jpg'),
+('el codigo da vinci', 3, 'dan brown', 11, 'El Código da Vinci es una novela de suspenso escrita por Dan Brown, esta obra aumentó el interés sobre la especulación del santo grial además cuestionó el papel que se le dio a María Magdalena en el c', 'images/davinci.jpg'),
+('la biblia', 4, 'armando j. levoratti', 4, 'libro del pueblo de dios', 'images/biblia.jpg'),
+('crepusculo', 5, 'stephenie meyer', 12, 'esta es la historia de Bella, una chica que luego de enamorarse descubre un mundo de vampiros completamente desconocido.', 'images/crepusculo.jpg');
 
 -- --------------------------------------------------------
 
@@ -97,13 +97,14 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_categoria`);
 
 --
 -- Indices de la tabla `libros`
 --
 ALTER TABLE `libros`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_libro`),
+  ADD KEY `libro_categoria` (`categoria`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -119,13 +120,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -141,7 +142,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `libros`
 --
 ALTER TABLE `libros`
-  ADD CONSTRAINT `categorias_libros` FOREIGN KEY (`id`) REFERENCES `categorias` (`id`);
+  ADD CONSTRAINT `libro_categoria` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
