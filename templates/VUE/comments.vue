dc9d740b56f481c8564a793c2f6c8b2f3a89ed72
gigenaMatias <matiasgigena12345@outlook.com>
2021-07-16 21:12:55 -0300

    finalizado :D

--- templates/VUE/comments.vue
+++ templates/VUE/comments.vue
@@ -1,10 +1,19 @@
 {literal}
     <div id="comentario">
-                  <div>
-                        <div v-for= "comentario in comentarios">
-                        {{comentario.comentario}}
-                        </div>
+            <div v-for= "comentario in comentarios">
+            <div v-if= "comentario.comentario === ''"></div>
+                  <div v-else>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                              <div class="ms-2 me-auto">
+                                    <div class="fw-bold">
+                                          {{comentario.usuario}}
+                                    </div>
+                              {{comentario.comentario}}
+                              </div>
+                              <span class="badge bg-primary rounded-pill">{{comentario.calificacion}}</span>
+                        </li>
                   </div>
+            </div>
       </div>
 {/literal}
 
--- templates/VUE/commentsadmin.vue
+++ templates/VUE/commentsadmin.vue
@@ -1,12 +1,21 @@
 {literal}
     <div id="comentario">
-                        <div v-for= "comentario in comentarios">
-                        <div v-if= "comentario.comentario === ''"></div>
-                        <div v-else>
-                              {{comentario.comentario}}
-                              <button v-on:click="borrarcomentario(comentario.id_comentario)" class="btn btn-outline-danger">Eliminar comentario</button>
+      <div v-for= "comentario in comentarios">
+      <div v-if= "comentario.comentario === ''"></div>
+            <div v-else>
+                  <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                              <div class="fw-bold">
+                                    {{comentario.usuario}}
+                              </div>
+                        {{comentario.comentario}}
                         </div>
+                        <span class="badge bg-primary rounded-pill">{{comentario.calificacion}}</span>
+                  </li>
+                  <div>
+                        <button v-on:click="borrarcomentario(comentario.id_comentario)" class="btn btn-outline-danger">Eliminar comentario</button>
                   </div>
+            </div>
+      </div>
       </div>
 {/literal}
-
